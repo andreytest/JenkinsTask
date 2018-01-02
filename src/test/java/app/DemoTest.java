c74d314816c1b0c2363355fd0373091260225720
Andrii Yavtushenko <Andrii_Yavtushenko@epam.com>
2018-01-02 11:54:50 +0200

    add task

---
+++ src/test/java/app/DemoTest.java
@@ -0,0 +1,18 @@
+package app;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DemoTest {
+    @Test
+    public void checkForPass(){
+        Assert.assertEquals("Test for jenkins", "Test for jenkins");
+    }
+
+    @Test
+    public void checkForFail(){
+        Assert.assertNotEquals("Test for jenkins","");
+    }
+
+}
