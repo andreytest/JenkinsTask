8569d422f417f2d02725ccb30d6679d23158b67d
Andrii Yavtushenko <Andrii_Yavtushenko@epam.com>
2018-01-30 14:20:46 +0200

    Add selenide

--- src/test/java/Tests/TestLogin.java
+++ src/test/java/Tests/TestLogin.java
@@ -36,63 +36,6 @@ public class TestLogin {
     }
 
     @Test
-    public void searchInCategory() {
-        MainPage mainPage = new MainPage();
-        ElementsCollection namesOfProductsOnPage = mainPage
-                .goToSubCategory("Электроника", "Мобильные телефоны")
-                .searchInCategory("iphone")
-                .getNamesOfProductsOnPage();
-        for (SelenideElement str : namesOfProductsOnPage) {
-            str.shouldHave(text("iphone"));
-        }
-    }
-
-
-    @Test
-    public void filterByPrice() {
-        String priceMin = "1000";
-        String priceMax = "3000";
-        Set<Boolean> havePriceOutCostRange = new HashSet<>();
-
-        MainPage mainPage = new MainPage();
-        mainPage
-                .goToSubCategory("Электроника", "Мобильные телефоны")
-                .filterByPrice(priceMin, priceMax)
-                .priceProductsOnPage()
-                .iterator()
-                .forEachRemaining(
-                        priceProduct -> {
-                            if (priceProduct < Integer.parseInt(priceMin) || priceProduct > Integer.parseInt(priceMax))
-                                havePriceOutCostRange.add(true);
-                        });
-        Assert.assertFalse(havePriceOutCostRange.contains(true));
-        ProductPage productPage = new ProductPage();
-        productPage.cancelFiltrByPrice();
-    }
-
-    @Test
-    public void sortByPriceAscending() {
-        MainPage mainPage = new MainPage();
-        List<Integer> actual = mainPage
-                .goToSubCategory("Электроника", "Мобильные телефоны")
-                .sortBy("цена от дешевых")
-                .priceProductsOnPage();
-        assertThat(actual).isSorted();
-    }
-
-    @Test
-    public void sortByPriceDescending() {
-        MainPage mainPage = new MainPage();
-        List<Integer> actual = mainPage
-                .goToSubCategory("Электроника", "Мобильные телефоны")
-                .sortBy("цена от дорогих")
-                .priceProductsOnPage();
-        List<Integer> expected = new ArrayList<>(actual);
-        Collections.sort(expected, Collections.reverseOrder());
-        Assert.assertTrue(expected.equals(actual));
-    }
-
-    @Test
     public void invalidLogin() {
         MainPage mainPage = new MainPage();
         LoginForm loginForm = mainPage.openLoginForm();
@@ -119,11 +62,5 @@ public class TestLogin {
         close();
     }
 
-    @Test
-    public void changeCity() {
-        MainPage mainPage = new MainPage();
-        mainPage.changeCity();
-        mainPage.changeCityButton().shouldHave(text("Днепр"));
-    }
 
 }
